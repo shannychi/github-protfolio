da09b527124d315081ca0bf9ace5acf50cfb8b0d
Edeh sharon <Pearllizzy196@gmail.com>
2024-04-17 17:42:55 +0100

    update link

--- src/component/repo.jsx
+++ src/component/repo.jsx
@@ -124,11 +124,13 @@ const GitHubRepo = () => {
                       {repo.name}
                     </h2>
                   </div>
-                  <Link to={/repository/repo.id}>
-                    <button className="relative py-2 px-8 text-black text-base font-bold nded-full overflow-hidden bg-white rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-blue-500 before:to-blue-300 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0 m-10">
-                      Home page
-                    </button>
-                  </Link>
+                  <a
+                    href={`/repository/${repo.id}`}
+                    className="block hover:bg-gray-300 bg-gray-200 text-gray-800 mt-6 rounded p-2 px-6"
+                    target="_blank"
+                  >
+                    Explore
+                  </a>
                 </div>
               ))}
             </div>
