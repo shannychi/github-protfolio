19756ff281798774b04e24e505eded5a19258280
Edeh sharon <Pearllizzy196@gmail.com>
2024-04-18 03:26:02 +0100

    updating link

--- src/component/repo.jsx
+++ src/component/repo.jsx
@@ -125,7 +125,7 @@ const GitHubRepo = () => {
                     </h2>
                   </div>
                   <Link
-                    to={`/repository/${repo.id}/`}
+                    to={`/repository/:${repo.id}/`}
                     className="block hover:bg-gray-300 bg-gray-200 text-gray-800 mt-6 rounded p-2 px-6"
                     target="_blank"
                   >
